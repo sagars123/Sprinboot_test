7c1dc2f83cbe089aae9cd33d5c19986a23926190
sagar.sriavstava@chaipoint.com <sagar.sriavstava@chaipoint.com>
2020-06-13 20:28:01 +0530

    support different delimeters

--- calculator.service/src/main/java/com/incubyte/calculator/service/StringCalculatorService.java
+++ calculator.service/src/main/java/com/incubyte/calculator/service/StringCalculatorService.java
@@ -5,15 +5,21 @@ public class StringCalculatorService {
   public int Add(String Number) {
     int sum = 0;
     char x = ' ';
+    char x1 = ' ';
     String s1 = "";
     if (Number.equals(" ")) {
       return 0;
     }
     for (int i = 0; i <Number.length(); i++) {
       x = Number.charAt(i);
-      if (x!='\n') {
+      if(i+1<Number.length())
+      x1 = Number.charAt(i+1);
+      
+     if (x!='\n'&& x!='/' && x!=';' && x!='\\' && x!='n' && x1!='n') {
+        System.out.print(x);
         s1 = s1 + x;
       } else {
+        if(!s1.equals(""))
         sum += Integer.parseInt(String.valueOf(s1));
         s1 = "";
       }
