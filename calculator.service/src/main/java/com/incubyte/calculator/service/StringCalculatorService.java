67a6c5be281a8656a1dec7de8b87b236ec07f483
sagar.sriavstava@chaipoint.com <sagar.sriavstava@chaipoint.com>
2020-06-13 13:18:34 +0530

    refactored and for dev for  N number

--- calculator.service/src/main/java/com/incubyte/calculator/service/StringCalculatorService.java
+++ calculator.service/src/main/java/com/incubyte/calculator/service/StringCalculatorService.java
@@ -1,21 +1,28 @@
 package com.incubyte.calculator.service;
 
 public class StringCalculatorService {
-  
+
   public int Add(String Number) {
     int sum = 0;
     char x = ' ';
+    String s1 = "";
     if (Number.equals(" ")) {
       return 0;
     }
-    for (int i = 0; i < Number.length(); i++) {
+    for (int i = 0; i <Number.length(); i++) {
       x = Number.charAt(i);
+      System.out.print(x);
       if (x != ',') {
-        sum += Integer.parseInt(String.valueOf(x));
+        s1 = s1 + x;
+      } else {
+        sum += Integer.parseInt(String.valueOf(s1));
+        s1 = "";
       }
+      
     }
+    if(!s1.equals(""))
+    sum += Integer.parseInt(String.valueOf(s1));
     return sum;
   }
-
 
 }
--- calculator.service/src/test/java/com/incubyte/calculator/test/service/StringCalculatorTestService.java
+++ calculator.service/src/test/java/com/incubyte/calculator/test/service/StringCalculatorTestService.java
@@ -9,11 +9,19 @@ import com.incubyte.calculator.service.StringCalculatorService;
 public class StringCalculatorTestService {
 
   @Test
-  public void testFor2Numbers()
-  {
-    StringCalculatorService stringCalculatorservice = new StringCalculatorService(); 
+  public void testFor2Numbers() {
+    StringCalculatorService stringCalculatorservice = new StringCalculatorService();
     assertEquals(0, stringCalculatorservice.Add(" "));
     assertEquals(1, stringCalculatorservice.Add("1,"));
-    assertEquals(3, stringCalculatorservice.Add("1,2"));
+    assertEquals(24, stringCalculatorservice.Add("1,23"));
   }
+
+
+  @Test
+  public void testForNumbers() {
+    StringCalculatorService stringCalculatorservice = new StringCalculatorService();
+    assertEquals(8, stringCalculatorservice.Add("1,2,2,3"));
+    assertEquals(280, stringCalculatorservice.Add("1,82,4,23,32,44,94"));
+  }
+
 }
